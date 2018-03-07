865a637bbcb3cb2d47c4d0b200ef9e7c351c0de6
allen <allen.yang@aliyun.com>
2018-03-07 20:58:44 +0800

    second commit

--- src/main/java/com/github/allen/Main.java
+++ src/main/java/com/github/allen/Main.java
@@ -2,6 +2,7 @@ package com.github.allen;
 
 public class Main {
     public static void main(String [] args){
+        System.out.println("ssss");
         System.out.println("hello world");
     }
 }
